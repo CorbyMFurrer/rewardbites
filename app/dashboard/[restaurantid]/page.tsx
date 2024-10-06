14c7199680e92e7886fb99061f8c7b4960b65432
CorbyMFurrer <corbfurrer@gmail.com>
2024-10-06 13:53:08 -0400

    page

--- app/dashboard/[restaurantid]/page.tsx
+++ app/dashboard/[restaurantid]/page.tsx
@@ -3,11 +3,7 @@ import React from "react";
 import Rewards from "@/components/Dashboard-Components/add-money/Rewards";
 import Balance from "@/components/Dashboard-Components/add-money/Balance";
 
-interface ViewRewardsProps {
-  selectedRestaurant: string;
-}
-
-export default function ViewRewards({ selectedRestaurant }: ViewRewardsProps) {
+export default function ViewRewards() {
   return (
     <section className="max-w-xl mx-auto space-y-6">
       <Rewards />
